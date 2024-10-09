ccc525439f8f9f91d86ce5e9d6ba03f70a5d09b5
Atif Ali <atif276ali@gmail.com>
2024-10-10 00:04:36 +0530

    feat(task-1) : Add Job to mutate DB every 2hr with coin details

--- crypto-backend/jobs/crypto.jobs.js
+++ crypto-backend/jobs/crypto.jobs.js
@@ -2,5 +2,31 @@ import CryptoData from "../models/crypto.schema.js";
 
 //! Cron Job to update data every 2 hours
 export default async function startCryptoJob() {
-  console.log("Running crypto data fetch job...");
+  runCryptoJob();
+  cron.schedule("0 */2 * * *", runCryptoJob);
+}
+
+async function runCryptoJob() {
+  try {
+    console.log("Running crypto data fetch job...");
+    const coinIds = "bitcoin,matic-network,ethereum";
+    const response = await axios.get(
+      `${process.env.COINGECKO_API_URL}?ids=${coinIds}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`
+    );
+    const data = response.data;
+    if (data) {
+      const coins = ["bitcoin", "matic-network", "ethereum"];
+      coins.forEach(async (coin) => {
+        const newData = new CryptoData({
+          coin: coin,
+          price: data[coin].usd,
+          marketCap: data[coin].usd_market_cap,
+          change24h: data[coin].usd_24h_change,
+        });
+        await newData.save();
+      });
+    }
+  } catch (error) {
+    console.log("Something Went Wrong: ", error);
+  }
 }
--- crypto-backend/server.js
+++ crypto-backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import connectToDB from "./utils/db.js";
+import startCryptoJob from "./jobs/crypto.jobs.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
@@ -9,6 +10,7 @@ async function init() {
   try {
     await connectToDB();
     await app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    await startCryptoJob();
   } catch (error) {
     console.log("Failed To start the server");
   }
